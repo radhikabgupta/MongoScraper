72fea0898a17d58b64d48c8d8300ec2ec2f446a6
Radhika Gupta <radhika.gupta@yahoo.com>
2018-06-17 16:30:29 -0400

    updt

---
+++ models/Article.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+
+  summary: {
+    type: String,
+    required: false,
+    unique: false
+  },  
+
+  date: String,
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
---
+++ server.js
@@ -0,0 +1,48 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+
+mongoose.Promise = Promise;
+var app = express();
+
+app.use(bodyParser.urlencoded({
+   extended: false
+}));
+
+app.use(express.static(process.cwd() + "/public"));
+
+var databaseUri = "mongodb://localhost/mongoosearticles";
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect(databaseUri);
+}
+
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection sucessful.");
+});
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var router = express.Router();
+
+require("./config/routes")(router);
+
+app.use(router);
+
+var port = process.env.PORT || 3030;
+
+app.listen(port, function() {
+  console.log("app running on port " + port);
+});
